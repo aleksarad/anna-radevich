ba9748b7032687279894021168447d4c2cd57165
aleksarad <alexa.radevich1@gmail.com>
2020-10-29 15:43:15 -0400

    added about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div id="about-content">
+        <div id="about-content" style={{textAlign: 'left'}}>
             <h2>Hi, I'm Anna!</h2>
             <p>I'm originally from Ukraine, currently residing in Brooklyn. I'm inspired by animals (my dog), nature (still my dog), and all things whimsical and cute (in conclusion, my dog). While I've mostly drawn on paper, I have recently begun exploring digital art, which you can see on the site. Thanks for visiting!</p>
             <h2>Get in touch:</h2>
